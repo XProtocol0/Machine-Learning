e19238a0a09e86c9d1270f772dddd243f0f7095f
XProtocol0 <tyagiaviral@gmail.com>
2020-04-22 00:31:38 +0530

    changes value of 10 to 0 and adds comments for further understanding

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -10,7 +10,14 @@ import predict
 
 data = loadmat('Data/ex3data1.mat')
 X = data['X']
-y = data['y']
+y = data['y'].ravel()
+y[y== 10] = 0
+
+m, _ = np.shape(X)
+
+y = y.reshape(m,1)
+
+print (y)
 
 _, axis = plt.subplots(5,5,figsize=(5,5))
 for i in range(5):
@@ -20,9 +27,22 @@ for i in range(5):
 plt.show()
 
 
-m = len(y)
+#m = len(y)
 ones = np.ones((m,1))
-X = np.hstack((ones, X)) #add the intercept
+X = np.hstack((ones, X)) #add the intercep
+
+'''
+Now X = [[1 _ _ _ _ _ ....]
+         [1 _ _ _ _ _ ....]
+         [1 _ _ _ _ _ ....]
+         ..
+         ..
+         ..
+         [1 _ _ _ _ _ ....]]
+with m x (n+1)
+
+'''
+
 #(m,n) = X.shape
 print(np.shape(X))
 #print(X)
@@ -33,6 +53,13 @@ greek_lambda = 0.1
 
 temp_theta = np.array([-2, -1, 1, 2]).reshape(4,1)
 temp_X = np.array([np.linspace(0.1, 1.5, 15)]).reshape(3,5).T
+'''
+w/0 T 
+temp_X= [[0.1 0.2 0.3 0.4 0.5]
+        [0.6 0.7 0.8 0.9 1. ]
+        [1.1 1.2 1.3 1.4 1.5]]
+
+'''
 temp_X = np.hstack((np.ones((5,1)), temp_X))
 temp_y = np.array([1, 0, 1, 0, 1]).reshape(5,1)
 J = costfunction.cost(temp_theta, temp_X, temp_y, 3)
@@ -42,13 +69,13 @@ grad = gradient.gradient(temp_theta, temp_X, temp_y, 3)
 print(grad)
 
 print("Running gradient descent...(estimated 5 seconds)")
-greek_lambda = 0.1
+greek_lambda = 1
 num_labels = 10
 all_theta, all_J = onevsall.onevall(X, y, num_labels, greek_lambda)
 
 plot_j.plot(all_J)
 
-
+print(np.shape(all_theta))
 
 
 prediction = predict.predict(all_theta, X)
