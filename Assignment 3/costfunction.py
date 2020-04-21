0ddc0bde0b753b2cda93c0b9e2b5c10f24550d99
XProtocol0 <tyagiaviral@gmail.com>
2020-04-22 00:33:28 +0530

    Adds comments

--- Assignment 3/costfunction.py
+++ Assignment 3/costfunction.py
@@ -4,8 +4,22 @@ import sigmoid
 
 def cost(theta, X, y, greek_lambda):
     m = len(y)
-    h = sigmoid.sig(X @ theta)
-    c = sum((y * np.log(h)) + ((1-y) * np.log(1-h)))/(-m)
+    h = sigmoid.sig(X @ theta) # This is vector multiplication. 
+    '''
+    h = [[ _ ]    each row is for 1 training example
+         [ _ ]
+         [ _ ]
+         [ _ ]
+         [ _ ]]
+
+    '''
+    c = sum((y * np.log(h)) + ((1-y) * np.log(1-h)))/(-m) 
+    '''
+    np.log, y * np.log(h)) are element wise and not vector operation.
+    sum() adds all the elements in the matrix.
+    '''
+
+
     regularized_c= c + greek_lambda/(2*m)*sum(theta[1:]**2)
     #print(np.shape(regularized_c))
     return regularized_c
--- Assignment 3/onevsall.py
+++ Assignment 3/onevsall.py
@@ -8,12 +8,18 @@ def onevall(X, y, num_labels, greek_lambda):
     all_theta = []
     all_J = []
 
-    X = np.hstack((np.ones((m,1)), X))
+    X = np.hstack((np.ones((m,1)), X)) # adding intercept term. 
 
     alpha = 1
     
     for i in range(1, num_labels+1):
         theta, J_history = gradientdes.graddes(initial_theta, X, np.where( y== i, 0.9, 0),alpha, 300, greek_lambda) 
+        '''
+         np.where( y== i, 0.9, 0)
+         when y==i returns 0.9 otherwise 0
+
+
+        '''
         all_theta.extend(theta)
         all_J.extend(J_history)
     
