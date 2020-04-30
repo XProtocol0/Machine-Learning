9c4849d7c9b8958e5b0a52dfbb45ceb5a6864806
XProtocol0 <tyagiaviral@gmail.com>
2020-04-30 15:38:34 +0530

    Removes newaxis addition to X and instead it's done in main.py

--- Assignment 5/linearRegCostFunction.py
+++ Assignment 5/linearRegCostFunction.py
@@ -3,16 +3,22 @@ import numpy as np
 
 def cost (theta, X, y, greek_lambda):
     m = len(y)
+   # ones = np.ones((m,1))
+   # X = np.hstack((ones,X))
     h = X @ theta
 
     c = sum((h-y)**2)/(2*m)
 
     regularized_c = c + greek_lambda/(2*m)*sum(theta[1:]**2)
+    
     return regularized_c
 
 def grad (theta, X, y, greek_lambda):   
-    h = X @ theta
     m = len(y)
+   # ones = np.ones((m,1))
+   # X = np.hstack((ones,X))
+    h = X @ theta
+    
     grad1 = 1/m * X.T @ (h - y)
     grad2 = 1/m * X.T @ (h - y) + (greek_lambda/m * theta)
     grad = np.vstack((grad1[0], grad2[1:]))
