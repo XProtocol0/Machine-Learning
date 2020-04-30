52a3fa2316de5b132f6761b189946a0c33638d95
XProtocol0 <tyagiaviral@gmail.com>
2020-04-30 15:39:31 +0530

    Adds newaxis addition to X

--- Assignment 5/main.py
+++ Assignment 5/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as op
 import linearRegCostFunction
-
+import learningCurve
 
 data = loadmat('Data/ex5data1.mat')
 
@@ -13,6 +13,8 @@ Xval = data['Xval']
 yval = data['yval']
 Xtest = data['Xtest']
 ytest = data['ytest']
+
+
 
 plt.scatter(X,y, marker='x', c ='r')
 plt.xlabel("Change in water level")
@@ -25,25 +27,24 @@ m = len(y)
 theta =np.ones((2,1))
 print(np.shape(theta))
 ones = np.ones((m,1))
-X = np.hstack((ones,X))
-print(X)
+X1 = np.hstack((ones,X))
+#print(X)
 
-J = linearRegCostFunction.cost(theta, X, y, 1)
-grad = linearRegCostFunction.grad(theta, X, y, 1)
+J = linearRegCostFunction.cost(theta, X1, y, 1)
+grad = linearRegCostFunction.grad(theta, X1, y, 1)
  
 
 print('Cost at theta = [1 ; 1]:', J)
 print("Gradient at theta = [1 ; 1]:",grad)
-#         '\n(this value should be about 303.993192)\n'], J);
+
 
 greek_lambda = 0
-temp = op.fmin_tnc(func = linearRegCostFunction.cost, x0 = theta.flatten(), fprime = linearRegCostFunction.grad, args = (X, y.flatten(), 0))
+temp = op.fmin_tnc(func = linearRegCostFunction.cost, x0 = theta.flatten(), fprime = linearRegCostFunction.grad, args = (X1, y.flatten(), 0))
 theta_optimized = temp[0]
 theta_optimized = theta_optimized[:, np.newaxis]
-#print(np.shape(theta_optimized))
 
 
-plt.scatter(X[:,1],y,marker="x",color="r")
+plt.scatter(X1[:,1:],y,marker="x",color="r")
 plt.xlabel("Change in water level")
 plt.ylabel("Water flowing out of the dam")
 x_value=[x for x in range(-50,40)]
@@ -52,3 +53,18 @@ plt.plot(x_value,y_value,color="b")
 plt.ylim(-5,40)
 plt.xlim(-50,40)
 plt.show()
+
+
+
+#Xval_1 = np.hstack((np.ones((21,1)),Xval))
+error_train, error_val = learningCurve.lc(X1, y, Xval, yval, greek_lambda)
+
+
+plt.plot(range(12),error_train,label="Train")
+plt.plot(range(12),error_val,label="Cross Validation",color="r")
+plt.title("Learning Curve for Linear Regression")
+plt.xlabel("Number of training examples")
+plt.ylabel("Error")
+plt.legend()
+plt.show()
+
